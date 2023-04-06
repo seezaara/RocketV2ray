3d6fe4b7d179892343ffc7a3331fe029716e270f
seezaara <123152498+seezaara@users.noreply.github.com>
2023-04-07 01:27:23 +0330

    Add files via upload

---
+++ www/index.js
@@ -0,0 +1,356 @@
+!function () {
+
+    try {
+        var userdata = JSON.parse(localStorage.getItem("userdata") || '')
+        utils.init({
+            handle: userdata.handle.split("\n"),
+            bypass: userdata.bypass.split("\n"),
+            dns: userdata.dns.split(","),
+            host: userdata.socks5.split(":")[0],
+            port: userdata.socks5.split(":")[1],
+            vpn: userdata.mode,
+        })
+    } catch (e) {
+        var userdata = { "language": "fa", "dns": "1.1.1.1,8.8.8.8", "socks5": "127.0.0.1:58694", "handle": "0.0.0.0/0", "bypass": "10.0.0.0/8\n172.16.0.0/16\n192.168.0.0/16", "mode": "on", "site": "on" }
+        localStorage.setItem("userdata", JSON.stringify(userdata))
+    }
+    setlanguage(userdata.language)
+
+    var button_parent = $(".button_parent")
+
+
+    load()
+    function load() {
+        var configs = utils.get_config()
+        var ind = utils.config_index()
+        var lists = ""
+        for (const i in configs) {
+            var config = utils.get_data_format(JSON.parse(configs[i]), false)
+            lists +=
+                `<div class="item ${ind == i ? "active" : ""}" data-ind="${i}">
+            <div class="item-color"></div>
+            <div class="item-title fillf">
+                <div class="title-name">${config.ps}</div>
+                <div class="title-ip">${config.add}:${config.port}</div>
+            </div>
+            <div class="item-share icon"></div> 
+            <div class="item-code icon"></div> 
+            <div class="item-delete icon"></div>
+        </div>`
+        }
+        $(".list").innerHTML = lists
+
+    }
+
+    $(".add").addEventListener("click", function () {
+        const text = clipboard.readText()
+        utils.add_config(text)
+        load()
+    })
+
+    $(".list").addEventListener("click", function (event) {
+        var ind = event.target.closest(".item")
+        if (!ind)
+            return
+        ind = ind.getAttribute("data-ind")
+        if (event.target.closest(".item-delete") && utils.config_index() != ind) {
+            utils.remove_config(ind)
+            load()
+        }
+        if (event.target.closest(".item-title")) {
+            if (cannot()) {
+                return utils.log(lang.m11)
+            }
+            utils.config_index(ind)
+            load()
+        }
+        if (event.target.closest(".item-share")) {
+            const text = utils.config_share(ind, false)
+            clipboard.writeText(text)
+            utils.log(lang.m7, "done")
+        }
+        if (event.target.closest(".item-code")) {
+            const text = utils.config_share(ind, true)
+            clipboard.writeText(text)
+            utils.log(lang.m7, "done")
+        }
+    })
+
+    $(".test").innerHTML = lang.m8
+
+    function cannot() {
+        return button_parent.classList.contains("wait") || button_parent.classList.contains("active")
+    }
+
+    $(".test").addEventListener("click", test)
+    var hasdelay
+    function test() {
+        $(".test").innerHTML = lang.m10
+        ping(function (e, p) {
+            if (e) {
+                $(".test").innerHTML = e
+            } else {
+                $(".test").innerHTML = lang.m9 + p + "ms"
+            }
+        })
+        if (hasdelay) {
+            clearTimeout(hasdelay)
+            hasdelay = undefined
+        }
+        hasdelay = setTimeout(() => {
+            $(".test").innerHTML = lang.m8
+            hasdelay = undefined
+        }, 15000);
+    }
+
+    $(".close").addEventListener("click", function () {
+        api("close")
+    })
+    $(".power").addEventListener("click", function () {
+        if (button_parent.classList.contains("wait")) {
+            return utils.log(lang.m6)
+        }
+        button_parent.classList.add("wait")
+        if (button_parent.classList.contains("active")) {
+            utils.stop(function (e) {
+                button_parent.classList.remove("wait")
+                if (!e) {
+                    button_parent.classList.remove("active")
+                }
+            })
+        } else {
+            utils.start(function (e) {
+                button_parent.classList.remove("wait")
+                if (!e) {
+                    button_parent.classList.add("active")
+                    test()
+                    if (userdata.site) {
+                        setTimeout(() => {
+                            openExternal("https://youtu.be/oLKcXNZdG2M");
+                        }, 3000);
+                    }
+                }
+            })
+        }
+    })
+    // ==================================================
+
+    $(".setting").addEventListener("click", function () {
+        if (cannot()) {
+            return utils.log(lang.m11)
+        }
+        var langs = ''
+        for (var i of languages) {
+            langs += '<option value="' + i + '"  ' + (userdata.language == i ? "selected" : '') + '>' + i + '</option>'
+        }
+        var p =
+            `<div style="width: 340px;">
+            <style>
+                .modal {
+                    background-color: var(--dialog) !important;
+                }
+    
+                .modal .title {
+                    color: var(--icon);
+                    width: 45px;
+                    height: 20px;
+                    font-size: 19px
+                }
+    
+                .modal td *:first-child {
+                    vertical-align: middle;
+                }
+    
+                .modal .material-icons {
+                    color: var(--icon);
+                }
+    
+                .modal {
+                    color: var(--text);
+                }
+    
+                .modal [dialog_close] {
+                    background-color: transparent;
+                }
+    
+                .modal input[type="text"],
+                .modal select {
+                    height: 35px;
+                    width: 95%;
+                    background-color: var(--item);
+                    border: none;
+                    border-radius: 5px;
+                    font-size: 16px;
+                    padding: 4px 8px;
+                    color: var(--text);  
+                    box-sizing: border-box;
+                }
+    
+                .modal textarea {
+                    width: 95%;
+                    background-color: var(--item);
+                    border: none;
+                    border-radius: 5px;
+                    font-size: 16px;
+                    padding: 4px 8px;
+                    color: var(--text);
+                    box-sizing: border-box;
+                }
+    
+                .modal input[type="submit"] ,
+                .modal input[type="button"],
+                .modal button {
+                    cursor: pointer;
+                    width: 100%;
+                    padding: 5px;
+                    background-color: var(--item);
+                    border: none;
+                    border-radius: 5px;
+                    font-size: 20px;
+                    color: var(--text);
+                }
+     
+    
+                /* // ===========================================switch */
+    
+                input[type="checkbox"][switch] {
+                    position: relative;
+                    display: inline-block;
+                    appearance: none;
+                    margin: 0;
+                    padding: 0;
+                    width: 3em;
+                    height: 1.7em;
+                    border-radius: 2em;
+                    cursor: pointer;
+                    background-color: var(--item);
+                    -webkit-transition: .4s;
+                    transition: .4s;
+                }
+    
+                input[type="checkbox"][switch]:checked {
+                    background-color: var(--active);
+                }
+                input[type="checkbox"][switch]:checked:before {
+                    background-color: white;
+                }
+    
+                input[type="checkbox"][switch]:before {
+                    border-radius: 50%;
+                    position: absolute;
+                    content: "";
+                    height: 1.3em;
+                    width: 1.3em;
+                    left: .2em;
+                    bottom: .2em;
+                    background-color: var(--icon);
+                    -webkit-transition: .1s;
+                    transition: .1s;
+                }
+    
+                input[type="checkbox"][switch]:checked:before {
+                    -webkit-transform: translateX(1.3em);
+                    -ms-transform: translateX(1.3em);
+                    transform: translateX(1.3em);
+                }
+            </style>
+            <div style="width: 30px;height: 30px;padding: 3px;margin: 15px;">
+                <span class="material-icons" dialog_close="">close</span>
+            </div>
+            <div class="erorrmessage"></div>
+            <div style="margin: 15px;">
+                <form action="" dialog_call="submit">
+                    <table>
+                        <tr>
+                            <td class="title">language</td>
+                            <td>
+                                <select name="language">
+                                ${langs}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="title">DNS</td>
+                            <td>
+                                <input type="text" placeholder="display name" name="dns" value="${userdata.dns}">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="title">socks5</td>
+                            <td>
+                                <input type="text" placeholder="display name" name="socks5" value="${userdata.socks5}">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="title">handle</td>
+                            <td>
+                                <textarea name="handle" id="" style=" resize: none;height:84px;">${userdata.handle}</textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="title">bypass</td>
+                            <td>
+                                <textarea name="bypass" id="" style=" resize: none;height:84px;">${userdata.bypass}</textarea>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td><input style="font-size: 13px;" switch type="checkbox" name="mode" ${userdata.mode ? "checked" : ''}>
+                            </td>
+                            <td>
+                                <label for="">
+                                    vpn mode
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><input style="font-size: 13px;" switch type="checkbox" name="site" ${userdata.site ? "checked" : ''}>
+                            </td>
+                            <td>
+                                <label for="">
+                                    show YouTube video after connected
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <br>
+                                <div class="alignv"><input type="button" value="reset setting" dialog_call="click"></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"> 
+                                <div class="alignv"><input type="submit" value="save"></div>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </div> `
+
+        dialog(p, function (e) {
+            if (e.value == "reset setting") {
+                localStorage.removeItem("userdata")
+            } else {
+                event.preventDefault();
+                var data = new FormData(e)
+                var object = {};
+                data.forEach((value, key) => {
+                    if (key.substring(key.length - 2) == '[]') {
+                        key = key.substring(0, key.length - 2)
+                        if (!Array.isArray(object[key])) {
+                            object[key] = [value];
+                            return
+                        }
+                        object[key].push(value)
+                        return;
+                    }
+                    object[key] = value;
+                });
+                dialog_close()
+                localStorage.setItem("userdata", JSON.stringify(object))
+            }
+            utils.reload()
+        })
+    })
+}()
---
+++ www/utils.js
@@ -0,0 +1,255 @@
+
+//======================================== debug 
+var debugconfig = get_config()
+// ======================================== update database for old versions app 
+if (typeof debugconfig[0] == "object") {
+    var newconfig = {}
+    for (const i in debugconfig) {
+        newconfig[i] = JSON.stringify(core.v2c.v2c({ data: debugconfig[i] }))
+    }
+    localStorage.setItem("configs", JSON.stringify(newconfig))
+}
+//========================================================================
+window.$ = function (a) {
+    return document.querySelector(a)
+}
+
+!function () {
+    if (document.readyState === "complete" || document.readyState === "interactive") setTimeout(ready, 1);
+    else document.addEventListener("DOMContentLoaded", ready);
+
+    function ready() {
+        var style = `
+        .dark-overlay {
+            width: 100%;
+            height: 100%;
+            background: #0000004d;
+            left: 0;
+            top: 0;
+            position: fixed;
+            z-index: 99;
+        }
+
+        .modal {
+            min-width: 30px;
+            left: 50%;
+            top: 50%;
+            /* margin: 73px auto; */
+            transform: translate(-50%, -50%);
+            position: absolute;
+            background: rgba(170, 170, 170, .88);
+            border-radius: 10px;
+            overflow: hidden;
+            z-index: 100;
+        }`;
+        document.body.insertAdjacentHTML("beforeend", '<style>' + style + '</style>')
+    }
+    var dialog_func;
+    window.dialog_close = function (...a) {
+        document.querySelector(".dark-overlay").remove()
+        document.querySelector(".modal").remove()
+    }
+    window.dialog_call = function (...a) {
+        if (a[0] instanceof Event) {
+            a = [this, document.querySelector(".modal"), a[0]]
+        }
+        if (dialog_func)
+            dialog_func(...a);
+    }
+    window.dialog = function (e, f) {
+        var element
+        if (e instanceof HTMLElement) {
+            element = e
+            e = ''
+        }
+        dialog_func = f
+        document.body.insertAdjacentHTML("beforeend", '<div class="dark-overlay"></div><div class="modal">' + e + '</div >')
+        var dialog = document.body.lastElementChild
+        if (element)
+            dialog.appendChild(element)
+        if (dialog.querySelector("[dialog_close]") != null) {
+            dialog.querySelectorAll("[dialog_close]").forEach(function (item) { item.addEventListener(item.getAttribute("dialog_close") || "click", dialog_close); /*item.removeAttribute("dialog_close")*/ })
+        }
+        if (dialog.querySelector("[dialog_call]") != null) {
+            dialog.querySelectorAll("[dialog_call]").forEach(function (item) { item.addEventListener(item.getAttribute("dialog_call") || "click", dialog_call); /*item.removeAttribute("dialog_call")*/ })
+        }
+        return dialog;
+    }
+
+}();
+
+
+!function () {
+    var childs = [];
+    var parent;
+    if (document.readyState === "complete" || document.readyState === "interactive") setTimeout(ready, 1);
+    else document.addEventListener("DOMContentLoaded", ready);
+
+    function ready() {
+        var style = `
+.pcmess {
+	pointer-events: none;
+	height: auto;
+	display: block !important;
+    position: fixed;
+    bottom: 0;
+    width:100%; 
+    z-index: 1000000000;  
+} 
+.cmess {    
+    pointer-events: none;
+    width: fit-content;
+    border-radius: 20px;
+    background: rgba(170, 170, 170, .88);
+    opacity: 0;
+    max-width: 90%;
+    padding: 4px 6px; 
+    transition: opacity 200ms;
+    font-size: 14px;
+    position: relative;
+    bottom: 40px;
+    flex-flow: row;
+    display: flex;
+    z-index: 1000000;  
+    margin: auto;
+    margin-bottom: 12px;
+}
+
+.cmess .cmess-icon {
+    font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 26px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: 'liga';
+    -webkit-font-smoothing: antialiased; 
+    padding: 0 2px; 
+}
+ 
+.cmess .cmess-content {
+    display:flex;
+    align-items:center;
+    text-align:center;
+    padding: 0 6px;
+	flex: 1;
+	line-height: 1.7;
+    z-index: 1000000;  
+}
+`
+        document.body.insertAdjacentHTML("beforeend", '<style>' + style + '</style><div class="pcmess"></div>')
+        parent = document.body.lastElementChild
+    }
+    window.mess = function (e, icon = "error") {
+        parent.insertAdjacentHTML("beforeend", '<div class="cmess" dir="auto"></div>')
+        var element = parent.lastElementChild
+        childs.push(element);
+        element.innerHTML = '<span class="cmess-content">' + e + '</span><span class="cmess-icon">' + icon + '</span>'
+        element.style.opacity = "1";
+        setTimeout(messClose, 1500 + (e.length * 60));
+    }
+    function messClose() {
+        var element = childs.shift()
+        element.style.opacity = "0";
+        setTimeout(function () {
+            element.remove();
+        }, 400);
+    }
+}()
+
+function add_config(a) {
+    try {
+        if (a.trim()[0] == "{") {
+            var config = core.v2c.v2c({ config: JSON.parse(a) })
+            if (!config)
+                return log(lang.m1)
+        } else if (a.includes("://")) {
+            var config = core.v2c.v2c({ url: a })
+            if (!config)
+                return log(lang.m1)
+        } else {
+            return log(lang.m1)
+        }
+    } catch (error) {
+        console.log(error)
+        return log(lang.m1)
+    }
+    var configs = get_config()
+    var key = Object.keys(configs)
+    key = ((+key[key.length - 1]) + 1) || 0
+    configs[key] = JSON.stringify(config)
+    localStorage.setItem("configs", JSON.stringify(configs))
+    log(lang.m3, "done")
+}
+function remove_config(i) {
+    var configs = get_config()
+    delete configs[i]
+    localStorage.setItem("configs", JSON.stringify(configs))
+}
+
+
+function get_config() {
+    try {
+        return JSON.parse(localStorage.getItem("configs") || "{}")
+    } catch (error) {
+        log(lang.m2)
+        localStorage.setItem("configs", "{}")
+        return get_config()
+    }
+}
+function config_index(ind) {
+    if (ind != undefined) {
+        localStorage.setItem("config_index", ind)
+    } else {
+        return localStorage.getItem("config_index") || 0
+    }
+}
+function config_share(ind, code) {
+    var config = get_config()[ind]
+    if (code)
+        return config
+    else
+        return core.c2v.c2v(JSON.parse(config))
+}
+// =============================================
+function start(cb) {
+    try {
+        var config = get_config()[config_index()]
+        if (!config) {
+            cb(true)
+            return log(lang.m5)
+        }
+        core.start(JSON.parse(config), function (e) {
+            if (e)
+                log(lang.m4)
+            cb(e)
+        })
+    } catch (error) {
+        log(e)
+    }
+}
+
+function reload() {
+    window.location.reload()
+}
+function log(a, b) {
+    mess(a, b)
+}
+var utils = {
+    add_config,
+    get_config,
+    get_data_format: core.c2v.c2v,
+    remove_config,
+    config_index,
+    config_share,
+    init: core.init,
+    start,
+    stop: core.stop,
+    log,
+    reload
+}
